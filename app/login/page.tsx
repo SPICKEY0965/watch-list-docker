43b8e5446a80be8156eca71875bf3a0b1e9ba023
SPICKEY0965 <137128214+SPICKEY0965@users.noreply.github.com>
2024-10-27 04:31:33 +0000

    [add] ログインページにトークンを取得し、ホームページにリダイレクトする機能を追加
    ローカルストレージからトークンを取得し、トークンが存在する場合にホームページにリダイレクトする機能を追加しました。
    これにより、ユーザーが再度ログインする必要がなくなり、ユーザーエクスペリエンスが向上します。

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,12 +1,20 @@
 "use client";
 
 import { LoginComponent } from "@/components/LoginComponent";
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useRouter } from 'next/navigation';  // useRouterをインポート
 
 export default function Page() {
     const [token, setToken] = useState<string | null>(null);
     const router = useRouter();  // useRouterフックを使用
+
+    useEffect(() => {
+        const storedToken = localStorage.getItem('token');
+        if (storedToken) {
+            setToken(storedToken);
+            router.push('/home');
+        }
+    }, []);
 
     const handleLogin = (newToken: string) => {
         setToken(newToken);
