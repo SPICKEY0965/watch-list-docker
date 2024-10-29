b380ef6a6ce59ec0b16d6f4f53aa8a9a699bfe47
SPICKEY0965 <137128214+SPICKEY0965@users.noreply.github.com>
2024-10-29 10:40:38 +0000

    [fix] 軽微な修正

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,7 +14,7 @@ const geistMono = localFont({
 });
 
 export const metadata: Metadata = {
-  title: 'WatchLis',
+  title: 'WatchList',
   description: 'ウォッチリストを管理するためのWEBアプリ',
   manifest: '/manifest.json',
 }
